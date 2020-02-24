3d2db4a49d7504cfa409f85e4f69bf6d193fa025
nickbadlose <nickgoodwin159@gmail.com>
2020-02-24 14:08:32 +0000

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api.router");
 const { pSQLErrors, generalErrors } = require("./errors/errors");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
