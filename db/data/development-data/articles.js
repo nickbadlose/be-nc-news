998fc3b01644c231668f8074bd43c35f982a4ebb
nickbadlose <nickgoodwin159@gmail.com>
2020-05-18 15:36:40 +0100

    edited dev data

--- db/data/development-data/articles.js
+++ db/data/development-data/articles.js
@@ -296,15 +296,15 @@ module.exports = [
     author: "grumpy19",
     body:
       "Manchester United Football Club is a professional football club based in Old Trafford, Greater Manchester, England, that competes in the Premier League, the top flight of English football. Nicknamed 'the Red Devils', the club was founded as Newton Heath LYR Football Club in 1878, changed its name to Manchester United in 1902 and moved to its current stadium, Old Trafford, in 1910.",
-    created_at: 1589803901544,
+    created_at: 1492124453987,
   },
   {
     title: "Is Brie the best cheese of them all?",
     topic: "cheese",
     author: "grumpy19",
     body:
-      "Brie (/briː/; French: [bʁi]) is a soft cow's-milk cheese named after Brie, the French region from which it originated (roughly corresponding to the modern département of Seine-et-Marne). It is pale in color with a slight grayish tinge under a rind of white mould. The rind is typically eaten, with its flavor depending largely upon the ingredients used and its manufacturing environment. It is similar to Camembert, which is native to a different region of France.",
-    created_at: 1589803912369,
+      "Brie is a soft cow's-milk cheese named after Brie, the French region from which it originated (roughly corresponding to the modern département of Seine-et-Marne). It is pale in color with a slight grayish tinge under a rind of white mould. The rind is typically eaten, with its flavor depending largely upon the ingredients used and its manufacturing environment. It is similar to Camembert, which is native to a different region of France.",
+    created_at: 1523660429836,
   },
   {
     title: "What happened to Shroud?",
@@ -312,7 +312,7 @@ module.exports = [
     author: "grumpy19",
     body:
       "So the mighty Shroud decided to pack his bags and leave Twitch to join Ninja over at mixer. I was just wondering, why did he leave?",
-    created_at: 1589805706549,
+    created_at: 1555196439846,
   },
   {
     title: "Save the Rhinos!!!",
@@ -320,134 +320,126 @@ module.exports = [
     author: "grumpy19",
     body:
       "Three of the five species in the family of Rhinocerotidae, are among the most endangered species in the world: the Black Rhino, the Javan Rhino and the Sumatran Rhino. The Javan Rhino is the most threatened with extinction with the total population of only 60 surviving in one National Park in Java, Indonesia. The Sumatran Rhino is Critically Endangered. It has been estimated that less than 100 exist today in the wild. The Black Rhino is classified by the IUCN as Critically Endangered with three subspecies declared extinct in 2011.",
-    created_at: 1589806567333,
+    created_at: 1455235298072,
   },
   {
     title: "Why guide dogs are just as important as ever!",
     topic: "dogs",
-    author: "grumpy19",
+    author: "jessjelly",
     body:
       "Guide dogs (colloquially known in the USA as seeing eye dogs[1]) are assistance dogs trained to lead blind and visually impaired people around obstacles. Although dogs can be trained to navigate various obstacles, they are red–green color blind and incapable of interpreting street signs. The human does the directing, based on skills acquired through previous mobility training. The handler might be likened to an aircraft's navigator, who must know how to get from one place to another, and the dog is the pilot, who gets them there safely. In several countries guide dogs, along with most service and hearing dogs, are exempt from regulations against the presence of animals in places such as restaurants and public transportation.",
-    created_at: 1589806752504,
+    created_at: 1486857619082,
   },
   {
     title: "Should cats be kept as house pets?",
     topic: "cats",
-    author: "grumpy19",
+    author: "jessjelly",
     body:
       "The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact. Domestic cats are valued by humans for companionship and their ability to hunt rodents. About 60 cat breeds are recognized by various cat registries.",
-    created_at: 1589806851286,
+    created_at: 1518393608263,
   },
   {
     title: "I'm travelling through spain next month, what are the must see's",
     topic: "travelling",
-    author: "grumpy19",
+    author: "jessjelly",
     body:
       "With an area of 505,990 km2 (195,360 sq mi), Spain is the largest country in Southern Europe, the second-largest country in Western Europe and the European Union, and the fourth-largest country by area on the European continent. With a population exceeding 46 million, Spain is the sixth-most populous country in Europe, and the fourth-most populous country in the European Union. Spain's capital and largest city is Madrid; other major urban areas include Barcelona, Valencia, Seville, Zaragoza, Málaga, and Bilbao.",
-    created_at: 1589807104956,
-  },
-  {
-    title: "I'm travelling through spain next month, what are the must see's",
-    topic: "travelling",
-    author: "grumpy19",
-    body:
-      "With an area of 505,990 km2 (195,360 sq mi), Spain is the largest country in Southern Europe, the second-largest country in Western Europe and the European Union, and the fourth-largest country by area on the European continent. With a population exceeding 46 million, Spain is the sixth-most populous country in Europe, and the fourth-most populous country in the European Union. Spain's capital and largest city is Madrid; other major urban areas include Barcelona, Valencia, Seville, Zaragoza, Málaga, and Bilbao.",
-    created_at: 1589807104956,
+    created_at: 1549929638472,
   },
   {
     title: "Did David Cameron abandon ship?",
     topic: "politics",
-    author: "grumpy19",
+    author: "weegembump",
     body:
       "David William Donald Cameron (born 9 October 1966) is a British Conservative Party politician who served as Prime Minister of the United Kingdom from 2010 to 2016. He was the Member of Parliament (MP) for Witney from 2001 to 2016 and leader of the Conservative Party from 2005 to 2016. He identifies as a one-nation conservative, and has been associated with both economically liberal and socially liberal policies.",
-    created_at: 1589807357247,
+    created_at: 1482624037485,
   },
   {
     title: "Joe wicks is the man!",
     topic: "fitness",
-    author: "grumpy19",
+    author: "weegembump",
     body:
       "His fitness method uses short intense High Intensity Interval Training (HIIT or, colloquially, 'HIT') workouts. Starting off by posting 15-second recipe videos on social media, Wicks grew his brand to become one of the most followed fitness accounts on Instagram and YouTube. His first published cookbook Lean in 15: 15-minute meals was a best-seller in 2015, having sold over 900,000 copies. He has created a '90 Day Plan' with workouts and meals with tailored portions to the individual. During the coronavirus pandemic lockdown, he began 'P.E With Joe' on YouTube to try to help children stay active; this livestream had a wide impact and was viewed by over a million users worldwide. He is a Channel 4 television presenter with his own show The Body Coach.",
-    created_at: 1589807481249,
+    created_at: 1545696029053,
   },
   {
     title: "I can't get enough of Stephen Hawking",
-    topic: "Physics",
-    author: "grumpy19",
+    topic: "physics",
+    author: "weegembump",
     body:
       "Stephen William Hawking CH CBE FRS FRSA (8 January 1942 – 14 March 2018) was an English theoretical physicist, cosmologist, and author who was director of research at the Centre for Theoretical Cosmology at the University of Cambridge at the time of his death. He was the Lucasian Professor of Mathematics at the University of Cambridge between 1979 and 2009.",
-    created_at: 1558306800000,
+    created_at: 1558306810584,
   },
   {
     title: "Why I think Band of Brother's is the best TV show of all time!",
     topic: "tv",
-    author: "grumpy19",
+    author: "cooljmessy",
     body:
       "Band of Brothers is a 2001 American war drama miniseries based on historian Stephen E. Ambrose's 1992 non-fiction book of the same name. The executive producers were Steven Spielberg and Tom Hanks, who also collaborated on the 1998 World War II film Saving Private Ryan and the 2010 World War II miniseries The Pacific. The episodes first started airing on HBO September 9, 2001. The series won Emmy and Golden Globe awards in 2001 for best miniseries.",
-    created_at: 1558306800000,
+    created_at: 1545696093045,
   },
   {
     title: "I watched the departed for the first time, wow!",
     topic: "films",
-    author: "grumpy19",
+    author: "cooljmessy",
     body:
       "The Departed is a 2006 American crime film directed by Martin Scorsese and written by William Monahan. It is a remake of the 2002 Hong Kong film Infernal Affairs. The Departed stars Leonardo DiCaprio, Matt Damon, Jack Nicholson, and Mark Wahlberg, with Martin Sheen, Ray Winstone, Vera Farmiga, and Alec Baldwin in supporting roles.",
-    created_at: 1558306800000,
+    created_at: 1577232010253,
   },
   {
     title: "The Romans were 100% the greatest architects of all time",
     topic: "architecture",
-    author: "grumpy19",
+    author: "cooljmessy",
     body:
       "Architecture (Latin architectura, from the Greek ἀρχιτέκτων arkhitekton 'architect', from ἀρχι- 'chief' and τέκτων 'creator') is both the process and the product of planning, designing, and constructing buildings or other structures. Architectural works, in the material form of buildings, are often perceived as cultural symbols and as works of art. Historical civilizations are often identified with their surviving architectural achievements.",
-    created_at: 1558306800000,
+    created_at: 1473462002835,
   },
   {
     title: "Our sun is not a big star",
     topic: "space",
-    author: "grumpy19",
+    author: "happyamy2016",
     body:
       "The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma, with internal convective motion that generates a magnetic field via a dynamo process. It is by far the most important source of energy for life on Earth. Its diameter is about 1.39 million kilometers (864,000 miles), or 109 times that of Earth, and its mass is about 330,000 times that of Earth. It accounts for about 99.86% of the total mass of the Solar System. Roughly three quarters of the Sun's mass consists of hydrogen (~73%); the rest is mostly helium (~25%), with much smaller quantities of heavier elements, including oxygen, carbon, neon, and iron.",
-    created_at: 1558306800000,
+    created_at: 1504998000072,
   },
   {
     title: "Pluto will always be a planet to me",
     topic: "pluto",
-    author: "grumpy19",
+    author: "happyamy2016",
     body:
       "Pluto (minor planet designation: 134340 Pluto) is an icy dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It was the first and the largest Kuiper belt object to be discovered.",
-    created_at: 1558306800000,
+    created_at: 1536534028936,
   },
   {
     title: "Dobby is the worst character in the show, discuss!",
     topic: "peepshow",
-    author: "grumpy19",
+    author: "happyamy2016",
     body:
       "Peep Show is a British television sitcom starring David Mitchell and Robert Webb. It was written by Jesse Armstrong and Sam Bain, with additional material by Mitchell and Webb, among others. It was broadcast on Channel 4 from 2003 until 2015. In 2010, it became the longest-running comedy in Channel 4 history in terms of years on air.",
-    created_at: 1558306800000,
+    created_at: 1568070028375,
   },
   {
     title: "Why ergonomics are very important to tech",
     topic: "tech",
-    author: "grumpy19",
+    author: "tickle122",
     body:
       "Human factors and ergonomics (commonly referred to as human factors) is the application of psychological and physiological principles to the engineering and design of products, processes, and systems. The goal of human factors is to reduce human error, increase productivity, and enhance safety and comfort with a specific focus on the interaction between the human and the thing of interest.",
-    created_at: 1558306800000,
+    created_at: 1463698800002,
   },
   {
     title: "Mclaren will always be the best cars!",
     topic: "cars",
-    author: "grumpy19",
+    author: "tickle122",
     body:
       "McLaren Racing Limited is a British motor racing team based at the McLaren Technology Centre, Woking, Surrey, England. McLaren is best known as a Formula One constructor and also has a history of competing in American open wheel racing as both an entrant and a chassis constructor, and has won the Canadian-American Challenge Cup (Can-Am) sports car racing championship. The team is the second oldest active, and second most successful Formula One team after Ferrari, having won 182 races, 12 Drivers' Championships and eight Constructors' Championships. The team is a wholly owned subsidiary of the McLaren Group.",
-    created_at: 1558306800000,
+    created_at: 1495234847583,
   },
   {
     title: "Final fantasy IX is just a big bundle of perfection and nostalgia",
     topic: "gaming",
-    author: "grumpy19",
+    author: "tickle122",
     body:
       "Final Fantasy IX is a 2000 role-playing video game developed and published by Squaresoft for the PlayStation video game console. It is the ninth game in the main Final Fantasy series. The plot centers on the consequences of a war between nations in a medieval fantasy world called Gaia. Players follow bandit Zidane Tribal, who kidnaps Alexandrian princess Garnet Til Alexandros XVII as part of a gambit by the neighboring nation of Lindblum. He joins Garnet and a growing cast of characters on a quest to take down her mother, Queen Brahne of Alexandria, who started the war. The plot shifts when the player learns that Brahne is a pawn of a more menacing threat, Kuja, who shares a mysterious history with Zidane spanning two worlds.",
-    created_at: 1558306800000,
+    created_at: 1526770800241,
   },
 ];
