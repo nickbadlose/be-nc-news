01c0a4a9533f3cf9b94712a0c564878698870470
nickbadlose <nickgoodwin159@gmail.com>
2020-06-02 00:41:33 +0100

    edited search to stop duplicating data

--- models/api.models.js
+++ models/api.models.js
@@ -12,30 +12,66 @@ exports.fetchAll = (search) => {
   return Promise.all([searchUsers, searchArticles, searchTopics]).then(
     ([user, articles, topic]) => {
       if (search.split(" ").length > 1) {
+        const matchIds = {};
+
         const titleMatches = articles.filter((article) => {
           return search.split(" ").every((word) => {
-            return article.title.toLowerCase().split(" ").includes(word);
+            if (article.title.toLowerCase().split(" ").includes(word)) {
+              matchIds.article.article_id = true;
+              return true;
+            }
           });
         });
+
         const bodyMatches = articles.filter((article) => {
           return search.split(" ").every((word) => {
-            return article.body.toLowerCase().split(" ").includes(word);
+            if (article.body.toLowerCase().split(" ").includes(word)) {
+              if ((matchIds.article.article_id = undefined)) {
+                matchIds.article.article_id = true;
+                return true;
+              }
+            }
           });
         });
+
         return [...topic, ...user, ...titleMatches, ...bodyMatches];
       } else {
+        const matchIds = {};
+
         const titleMatches = articles.filter((article) => {
-          return article.title.toLowerCase().split(" ").includes(search);
+          if (article.title.toLowerCase().split(" ").includes(search)) {
+            matchIds.article.article_id = true;
+            return true;
+          }
         });
+
         const authorMatches = articles.filter((article) => {
-          return article.author.toLowerCase() === search;
+          if (article.author.toLowerCase() === search) {
+            if ((matchIds.article.article_id = undefined)) {
+              matchIds.article.article_id = true;
+              return true;
+            }
+          }
         });
+
         const bodyMatches = articles.filter((article) => {
-          return article.body.toLowerCase().split(" ").includes(search);
+          if (article.body.toLowerCase().split(" ").includes(search)) {
+            if ((matchIds.article.article_id = undefined)) {
+              matchIds.article.article_id = true;
+              return true;
+            }
+          }
         });
+
         const topicMatches = articles.filter((article) => {
-          return article.topic.toLowerCase() === search;
+          if (article.topic.toLowerCase() === search) {
+            if ((matchIds.article.article_id = undefined)) {
+              matchIds.article.article_id = true;
+              return true;
+            }
+          }
         });
+
         return [
           ...topic,
           ...user,
