06aff97fc25345b046f88711ac0ce4e574341d81
nickbadlose <nickgoodwin159@gmail.com>
2020-06-07 23:54:16 +0100

    fixed user passwords, added default avatar url and removed expired avatars

--- db/migrations/20200203113803_users.js
+++ db/migrations/20200203113803_users.js
@@ -1,7 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (usersTable) => {
     usersTable.string("username").unique().primary().notNullable();
-    usersTable.string("avatar_url");
+    usersTable
+      .string("avatar_url")
+      .defaultTo(
+        "https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg"
+      );
     usersTable.timestamp("joined", { precision: 6 }).defaultTo(knex.fn.now(6));
     usersTable.string("name").notNullable();
     usersTable.string("password").notNullable();
