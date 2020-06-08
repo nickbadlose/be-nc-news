8a20a7faf0c2fecb9b3c6e2a7983679d9ff161df
nickbadlose <nickgoodwin159@gmail.com>
2020-06-08 14:46:21 +0100

    changed default pic to a library background

--- db/migrations/20200203112711_topics.js
+++ db/migrations/20200203112711_topics.js
@@ -5,27 +5,27 @@ exports.up = function (knex) {
     topicsTable
       .string("image_url")
       .defaultTo(
-        "https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&fm=jpg&w=400&fit=crop&ar=1:1"
+        "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&fm=jpg&w=400&fit=crop&ar=1:1"
       );
     topicsTable
       .string("image_thumb")
       .defaultTo(
-        "https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&fm=jpg&w=200&fit=crop&ar=1:1"
+        "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&fm=jpg&w=200&fit=crop&ar=1:1"
       );
     topicsTable
       .string("image_banner")
       .defaultTo(
-        "https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=1300&h=400&fit=crop&crop=edges"
+        "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=1300&h=400&fit=crop&crop=edges"
       );
     topicsTable
       .string("mobile_banner")
       .defaultTo(
-        "https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=600&h=200&fit=crop&crop=edges"
+        "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=600&h=200&fit=crop&crop=edges"
       );
     topicsTable
       .string("image_card")
       .defaultTo(
-        "https://images.unsplash.com/photo-1529243856184-fd5465488984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=400&h=226&fit=crop&crop=edges"
+        "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=400&h=226&fit=crop&crop=edges"
       );
   });
 };
